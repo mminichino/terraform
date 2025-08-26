612b808d2eb9f1fdf00c3a0b02b77e5b9912c24d
Michael Minichino <michael@minichino.co>
2025-08-25 21:53:23 -0500

    first commit

--- redis/testdb/modules/redis/main.tf
+++ redis/testdb/modules/redis/main.tf
@@ -65,6 +65,27 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${local.name_prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "default" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+
+  tags = {
+    Name = "${local.name_prefix}-rt"
+  }
+}
+
 resource "aws_subnet" "subnets" {
   count                   = length(data.aws_availability_zones.zones.names)
   vpc_id                  = aws_vpc.vpc.id
@@ -77,6 +98,13 @@ resource "aws_subnet" "subnets" {
     Name = "${local.name_prefix}-subnet-${data.aws_availability_zones.zones.names[count.index]}"
     Type = "public"
   }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.subnets)
+
+  subnet_id      = aws_subnet.subnets[count.index].id
+  route_table_id = aws_route_table.default.id
 }
 
 resource "aws_security_group" "redis_sg" {
