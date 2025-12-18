c4db6166f0dd81b5833f44a233a761855303fbcb
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2025-12-18 14:16:35 -0600

    Updated AWS modules

--- redis/aws/modules/database/main.tf
+++ redis/aws/modules/database/main.tf
@@ -6,34 +6,49 @@ resource "random_string" "password" {
 }
 
 locals {
-  data_json = jsonencode({
-    uid                                     = var.uid
-    memory_size                             = var.memory_size
-    name                                    = var.name
-    port                                    = var.port
-    proxy_policy                            = var.proxy_policy
-    shards_count                            = var.shards_count
-    shards_placement                        = var.shards_placement
-    type                                    = var.database_type
-    data_persistence                        = var.data_persistence
-    aof_policy                              = var.aof_policy
-    oss_cluster                             = var.oss_cluster
-    oss_cluster_api_preferred_endpoint_type = var.oss_cluster_endpoint
-    oss_cluster_api_preferred_ip_type       = var.oss_cluster_type
-    replication                             = var.replication
-    eviction_policy                         = var.eviction ? "volatile-lru" : "noeviction"
-    authentication_redis_pass               = random_string.password.id
-    module_list      = [
+  data_json = jsonencode(
+    merge(
       {
-        module_name = "search"
-        module_args = ""
+        uid                                     = var.uid
+        memory_size                             = var.memory_size
+        name                                    = var.name
+        port                                    = var.port
+        proxy_policy                            = var.proxy_policy
+        shards_count                            = var.shards_count
+        sharding                                = var.shards_count > 1
+        shards_placement                        = var.shards_placement
+        type                                    = var.database_type
+        data_persistence                        = var.data_persistence
+        aof_policy                              = var.aof_policy
+        oss_cluster                             = var.oss_cluster
+        oss_cluster_api_preferred_endpoint_type = var.oss_cluster_endpoint
+        oss_cluster_api_preferred_ip_type       = var.oss_cluster_type
+        replication                             = var.replication
+        eviction_policy                         = var.eviction ? "volatile-lru" : "noeviction"
+        authentication_redis_pass               = random_string.password.id
+        module_list = [
+          {
+            module_name = "search"
+            module_args = ""
+          },
+          {
+            module_name = "ReJSON"
+            module_args = ""
+          }
+        ]
       },
-      {
-        module_name = "ReJSON"
-        module_args = ""
-      }
-    ]
-  })
+      var.shards_count > 1 ? {
+        shard_key_regex = [
+          {
+            "regex": ".*\\{(?<tag>.*)\\}.*"
+          },
+          {
+            "regex": "(?<tag>.*)"
+          }
+        ]
+      } : {}
+    )
+  )
 }
 
 resource "null_resource" "database" {
