877ebfafb258d2002f8e36ea1ced60761c908da2
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2026-01-13 15:28:57 -0600

    Updated GCP modules

--- redis/gcp/modules/gke_env/versions.tf
+++ redis/gcp/modules/gke_env/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     helm = {
       source = "hashicorp/helm"
-      version = "3.0.2"
+      version = "3.1.1"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "2.38.0"
+      version = "3.0.1"
     }
   }
   required_version = ">= 0.14"
