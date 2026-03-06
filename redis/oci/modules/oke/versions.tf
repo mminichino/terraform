ccc465746270983037baec341760997ad17c5787
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2026-03-06 17:15:33 -0600

    Updated Cloud modules

--- redis/oci/modules/oke/versions.tf
+++ redis/oci/modules/oke/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     oci = {
       source = "oracle/oci"
-      version = "7.29.0"
+      version = "8.4.0"
     }
   }
 }
--- redis/oci/modules/oke_env/versions.tf
+++ redis/oci/modules/oke_env/versions.tf
@@ -10,7 +10,7 @@ terraform {
     }
     oci = {
       source = "oracle/oci"
-      version = "7.29.0"
+      version = "8.4.0"
     }
   }
   required_version = ">= 0.14"
